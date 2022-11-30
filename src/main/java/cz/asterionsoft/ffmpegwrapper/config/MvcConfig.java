bc5e3f1be9e87e9c9eb51db7512eb88ad8c668dd
Jan Marcis <jan.marcis7@gmail.com>
2022-11-30 16:23:58 +0100

    cleaning. static files

---
+++ src/main/java/cz/asterionsoft/ffmpegwrapper/config/MvcConfig.java
@@ -0,0 +1,21 @@
+package cz.asterionsoft.ffmpegwrapper.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
+
+@Configuration
+@EnableWebMvc
+public class MvcConfig implements WebMvcConfigurer {
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+				.addResourceHandler("/static/**")
+				.addResourceLocations("/static/")
+				.setCachePeriod(3600)
+				.resourceChain(true)
+				.addResolver(new PathResourceResolver());
+	}
+}
